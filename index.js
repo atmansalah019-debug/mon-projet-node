509173c13e6ef1c99a1873c492b3cc405cb0e401
atmansalah019-debug <atmansalah019@gmail.com>
2026-04-20 20:06:01 +0100

    Ajout de la fonction soustraction

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 "console.log('Hello Node');" 
+// index.js (branche feature) 
+function addition(a, b) { 
+return a + b; 
+} 
+function soustraction(a, b) { 
+return a - b; 
+}
+if (require.main === module) { 
+console.log("Résultat addition:", addition(5, 3)); 
+console.log("Résultat soustraction:", soustraction(5, 3)); 
+} 
+module.exports = { addition, soustraction }; 
